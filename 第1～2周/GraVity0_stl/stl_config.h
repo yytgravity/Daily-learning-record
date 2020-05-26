8ae11ab43777eda4d1d46290f6deffabc3e890c9
yytgravity <552018475@qq.com>
2020-05-27 01:18:28 +0800

    Create stl_config.h

---
+++ 第1～2周/GraVity0_stl/stl_config.h
@@ -0,0 +1,12 @@
+//
+// Created by GraVity0 on 2020/5/6.
+//
+
+#ifndef YYT_STL_STL_CONFIG_H
+#define YYT_STL_STL_CONFIG_H
+
+#define __STL_BEGIN_NAMESPACE namespace yyt_stl{
+#define __STL_END_NAMESPACE }
+
+
+#endif //YYT_STL_STL_CONFIG_H
