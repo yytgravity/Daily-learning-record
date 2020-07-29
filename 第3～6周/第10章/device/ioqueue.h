9f8b13338befd708f26a7fa871cc14405d7a8b2b
yytgravity <552018475@qq.com>
2020-07-29 22:22:00 +0800

    Update ioqueue.h

--- 第3～6周/第10章/device/ioqueue.h
+++ 第3～6周/第10章/device/ioqueue.h
@@ -6,21 +6,23 @@
 
 #define bufsize 64
 
-/* 环形队列 */
-struct ioqueue {
-// 生产者消费者问题
+//环形队列
+struct ioqueue{
     struct lock lock;
- /* 生产者,缓冲区不满时就继续往里面放数据,
-  * 否则就睡眠,此项记录哪个生产者在此缓冲区上睡眠。*/
     struct task_struct* producer;
-
- /* 消费者,缓冲区不空时就继续从往里面拿数据,
-  * 否则就睡眠,此项记录哪个消费者在此缓冲区上睡眠。*/
     struct task_struct* consumer;
-    char buf[bufsize];			    // 缓冲区大小
-    int32_t head;			    // 队首,数据往队首处写入
-    int32_t tail;			    // 队尾,数据从队尾处读出
+    char buf[bufsize];
+    int32_t head;
+    int32_t tail;
 };
+/*
+ lock 是本缓冲区的锁，每次对缓冲区操作时都要先申请这个锁，从而保证缓冲区操作互斥。
+ producer 是生产者，此项来记录当缓冲区满时，在此缓冲区睡眠的生产者线程。
+ consumer 是消费者，此项来记录当缓冲区空时，在此缓冲区睡眠的消费者线程。
+ buf[bufsize]是定义的缓冲区数组，其大小为 bufsize，在上面用 define 定义为 64。
+ head 是缓冲区队列的队首地址，tail 是队尾地址。
+ */
+
 
 void ioqueue_init(struct ioqueue* ioq);
 bool ioq_full(struct ioqueue* ioq);
