0a778823879316754f1052a85d91538f53afb584
wumingzhilian <69114599+wumingzhilian@users.noreply.github.com>
2021-10-04 21:42:17 +0800

    fix
    fix

--- week1-2/GraVity0_stl/main.cpp
+++ week1-2/GraVity0_stl/main.cpp
@@ -1,59 +1,11 @@
-#include <iostream>
-#include <deque>
-using namespace std;
-int main()
-{
-    deque<int>d;
-    d.push_back(1);
-    auto first = d.begin();
-    cout << *first << endl;
-    //添加元素，会导致 first 失效
-    d.push_back(2);
-    cout << *first << endl;
-    return 0;
-}
-
-
-/*
-#include <iostream>
-#include<vector>
-#include<list>
-#include<deque>
-#include <queue>
-#include <stack>
-#include<cstdio>
-#include<cstdlib>
-#include<unistd.h>
-#include<cstring>
-using namespace::std;
-
-int main()
-{
- 
-    // Creating a sample vector
-    vector <int> v = {1, 5, 10, 15, 20};
- 
-    // Changing vector while iterating over it
-    // (This causes iterator invalidation)
-    for (auto it = v.begin(); it != v.end(); it++)
-        if ((*it) == 5)
-            v.push_back(-1);
- 
-    for (auto it = v.begin(); it != v.end(); it++)
-        cout << (*it) << " ";
-         
-    return 0;
-}
-*/
-
-/*
-#include<iostream>
 #include "stl_vector.h"
+using namespace yyt_stl;
 int main() {
-    // 创建含整数的 list
-    yyt_stl::vector<int> v(5);
-//    sakura_stl::list<int> l2(l);
-    for (auto it = v.begin(); it != v.end(); it++)
-            std::cout << (*it) << " ";
+	vector<int> v2(10);
+	printf("%d", v2.capacity());
+	for (int i = 0; i < 10; i++)
+	{
+		v2.push_back(i);
+	}
+	return 0;
 }
-*/
